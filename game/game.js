12366346eee6a4a0f4ea8940c501211219ed46cf
Andre Nanninga <ag.nanninga@gmail.com>
2014-09-28 17:34:19 +0300

    basic perspective 3d

--- game/game.js
+++ game/game.js
@@ -1,25 +1,38 @@
-'use strict';
+// 'use strict';
 
 var Tree = require('./tree');
 
 var Game = {};
 
+Game.width = 800;
+Game.height = 500;
+
+Game.perspective = 500;
+Game.origin = {
+  x: Game.width / 2,
+  y: 0
+};
+
+Game.camera = {
+  x: 0,
+  y: 40,
+  z: 0
+};
+
 Game.init = function(container) {
   Game.stage = new PIXI.Stage(0xddffff);
-  Game.renderer = PIXI.autoDetectRenderer(800, 500);
+  Game.renderer = PIXI.autoDetectRenderer(Game.width, Game.height, null, false, true);
 
   Game.container = container;
   Game.container.appendChild(Game.renderer.view);
 
-  Game.tree = new Tree(Game.stage);
-  Game.stage.addChild(Game.tree.graphics);
+  Game.background = new PIXI.Graphics();
+  Game.background.beginFill(0x44ff44);
+  Game.background.drawRect(0, 100, Game.width, Game.height);
+  Game.background.endFill();
+  Game.stage.addChildAt(Game.background, 0);
 
-  Game.graphics = new PIXI.Graphics();
-  Game.graphics.beginFill(0xff0000);
-  Game.graphics.drawRect(10, 10, 100, 100);
-  Game.graphics.endFill();
-
-  Game.stage.addChild(Game.graphics);
+  Game.trees = [];
 
   requestAnimFrame(Game.tick);
 };
@@ -27,11 +40,35 @@ Game.init = function(container) {
 Game.tick = function() {
   requestAnimFrame(Game.tick);
 
-  Game.graphics.position.x += 1;
-  Game.graphics.position.y  += 1;
+  for(var i = 0; i < Game.trees.length; i++) {
+    Game.trees[i].tick(); 
+  }
 
+  if(Math.random() < 0.1 && Game.trees.length < 30) {
+
+    var tree = new Tree(Game.stage);
+    Game.stage.addChildAt(tree.graphics, 1);
+
+    tree.x = Game.random(-300, 300);
+    tree.y = 100;
+    tree.z = Game.camera.z - 50;
+
+    Game.trees.push(tree);
+  }
+
+  if(Game.trees.length >= 30) {
+    var deleted = Game.trees.splice(0, 1);
+
+    Game.stage.removeChild(deleted[0].graphics);
+  }
+
+  Game.camera.z += 2;
 
   Game.renderer.render(Game.stage);
+};
+
+Game.random = function(min, max) {
+  return Math.floor(Math.random()*(max-min+1)+min);
 };
 
 module.exports = Game;
--- game/tree.js
+++ game/tree.js
@@ -1,20 +1,90 @@
 'use strict';
 
+var _ = require('underscore');
+
 var Tree = function() {
   this.graphics = new PIXI.Graphics();
-  // this.graphics.position.x = 50;
-  // this.graphics.position.y = 50;
 
-  this.graphics.beginFill(0x00ff00);
+  this.width = 60;
 
-  // this.graphics.drawRect(0, 0, 200, 200);
+  this.x = 0;
+  this.y = 0; 
+  this.z = 0;
 
-  this.graphics.moveTo(0, 0);
-  this.graphics.lineTo(20, 60);
-  this.graphics.lineTo(-20, 60);
-  this.graphics.lineTo(0, 0);
-  
-  this.graphics.endFill();
+  this.alpha = 0;
+
+  var colors = [
+    [0xC88821, 0xB0781D],
+    [0xEABF2D, 0xD1AB28],
+    [0xBF5E26, 0xA65221],
+    [0x5F361D, 0x452715],
+    [0xA63921, 0x8F311C]
+  ];
+
+  this.colors = _.sample(colors);
+
+  this.faces = [
+    {
+      color: this.colors[0],
+      points: [
+        { x:   0, y:   0, z:   0 },
+        { x:  10, y:  60, z:   0 },
+        { x:   0, y:  60, z: -10 },
+        { x:   0, y:   0, z:   0 },
+      ]
+    },
+    {
+      color: this.colors[1],
+      points: [
+        { x:   0, y:   0, z:   0 },
+        { x: -10, y:  60, z:   0 },
+        { x:   0, y:  60, z: -10 },
+        { x:   0, y:   0, z:   0 },
+      ]
+    }
+  ];
+
+  this.graphics.hitArea = new PIXI.Rectangle(-20, 0, 40, 60);
+};
+
+Tree.prototype.tick = function() {
+  this.graphics.clear();
+
+  if(this.alpha < 1) {
+    this.alpha += 0.05;
+  }
+
+  if(this.z - Game.camera.z < -400) {
+    return;
+  }
+
+  for(var i = 0; i < this.faces.length; i++) {
+    var face = this.faces[i];
+
+    this.graphics.beginFill(face.color, this.alpha);
+
+    for(var j = 0; j < face.points.length; j++) {
+      var point = face.points[j];
+
+      var x = point.x + this.x - Game.camera.x;
+      var y = point.y + this.y - Game.camera.y;
+      var z = point.z + this.z - Game.camera.z;
+
+      var scale = Game.perspective / (Game.perspective + z);
+
+      var _x = (x * scale) + Game.origin.x;
+      var _y = (y * scale) + Game.origin.y;
+
+      if(j == 0) {
+        this.graphics.moveTo(_x, _y);
+      }
+      else {
+        this.graphics.lineTo(_x, _y);
+      }
+    }
+
+    this.graphics.endFill();
+  }
 };
 
 module.exports = Tree;
